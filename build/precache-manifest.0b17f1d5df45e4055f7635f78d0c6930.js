fe2c02ad84556970627362e62bbb7b209327fdb3
Leroy Salih <Leroy.Salih@gmail.com>
2020-02-26 09:38:22 +0300

    Adding Build Directory

---
+++ build/precache-manifest.0b17f1d5df45e4055f7635f78d0c6930.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a1253b38b84ede6761986aa7ab0307",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1edc6b6742ad12545857",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5796e6af70676b3189d7",
+    "url": "/static/js/2.aed5d769.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.aed5d769.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1edc6b6742ad12545857",
+    "url": "/static/js/main.cd4b2fc9.chunk.js"
+  },
+  {
+    "revision": "04e8c2708113a4e1e192",
+    "url": "/static/js/runtime-main.629d3d39.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
